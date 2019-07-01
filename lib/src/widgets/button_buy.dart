f64b8901fcbd3a893527667972df0b0d8f667ea6
Frank Moreno <frankmoreno1993@gmail.com>
2019-07-01 17:27:38 -0500

    code formatting

--- lib/src/widgets/button_buy.dart
+++ lib/src/widgets/button_buy.dart
@@ -3,13 +3,19 @@ import 'package:flutter/material.dart';
 class ButtonBuy extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return InkWell(onTap: (){
-      Navigator.pushNamed(context, 'details');
-    },
-          child: Container(
-      height: MediaQuery.of(context).size.height *0.11,
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, 'details');
+      },
+      child: Container(
+        height: MediaQuery.of(context).size.height * 0.11,
         width: MediaQuery.of(context).size.width,
-        child: Center(child: Text("SHOW DETAILS",style: TextStyle(color: Colors.white,fontSize: 20),)),
+        child: Center(
+          child: Text(
+            "SHOW DETAILS",
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
       ),
     );
   }
